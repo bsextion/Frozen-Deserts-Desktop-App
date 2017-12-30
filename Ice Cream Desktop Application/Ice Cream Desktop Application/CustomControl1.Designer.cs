74d54eb9cf41fbac764a2d3badee32bafd9e0f4b
bsextion <bsextion@gmail.com>
2017-12-30 00:32:50 -0600

    Add files via upload

---
+++ Ice Cream Desktop Application/Ice Cream Desktop Application/CustomControl1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Ice_Cream_Desktop_Application
+{
+    partial class CustomControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(47, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 147);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "$1";
+            this.label1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 67);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sundaes";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(41, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "See Menu";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-5, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(450, 87);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Choose 1 Flavor, 1 Topping, and 1 Syrup\r\n\r\n\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 75);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Over 20 different Flavors!\r\nOver 20 Toppings!\r\nOver 5 Syrups!\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Ice_Cream_Desktop_Application.Properties.Resources.sundae1;
+            this.pictureBox2.Location = new System.Drawing.Point(451, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(304, 354);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // CustomControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomControl1";
+            this.Size = new System.Drawing.Size(773, 382);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Ice Cream Desktop Application/Ice Cream Desktop Application/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ice_Cream_Desktop_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            customControl11.BringToFront();
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void customControl11_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void homeBtn_Click(object sender, EventArgs e)
+        {
+            customControl11.BringToFront();
+            
+        }
+
+        private void specialBtn_Click(object sender, EventArgs e)
+        {
+            customControl21.BringToFront();
+            
+        }
+
+        private void customControl21_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
